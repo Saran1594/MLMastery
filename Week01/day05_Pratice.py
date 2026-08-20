705d4af49eb3e43844cef6ab2bd89e966888d99a
Saravanan Murugan <mech.saravananm@gmail.com>
2026-08-20 10:50:21 -0400

    Week 1 Day 5: Online Calculator pratice program

---
+++ Week01/day05_Pratice.py
@@ -0,0 +1,27 @@
+# Online Calculator
+session = "True"
+while session == "True":
+    num1 = int(input("Input number 1\n"))
+    num2 = int(input("Input number 2\n"))
+    operations = ["+","-","/","%","*"]
+    choose = input("choose operation\n")
+    if choose in operations:
+        if choose == "+":
+            print(num1 + num2)
+        elif choose == "-":
+            print(num1 - num2)
+        elif choose == "/":
+            try:
+              print(num1 / num2)
+            except ZeroDivisionError:
+              print("Cannot divide by zero")
+        elif choose == "%":
+            print(num1%num2)
+        elif choose == "*":
+            print(num1*num2)
+        action = input("Do you want to continue")
+        session = action
+    else:
+        print("Invalid Operator")
+
+
