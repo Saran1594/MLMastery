5cbbf15d9693b339e638bb207e5542dc6bcd3d44
Saravanan Murugan <mech.saravananm@gmail.com>
2026-08-25 13:36:41 -0400

    Week2: Day 1 Nested Dict

---
+++ Week2/Day01.py
@@ -0,0 +1,17 @@
+# Creating Nested Dictionaries
+pph_tracker = {"A1":{"Shift":"1st","PPH":145,"Target":125},"A2":{"Shift":"2nd"}}
+pph_tracker["A2"]["PPH"] = 160
+pph_tracker["A2"]["Target"] = 110
+for key,value in pph_tracker.items():
+    print(key,value)
+def add_zone(zone_id,shift,pph,target_pph):
+    pph_tracker[zone_id] = {"Shift":shift,"PPH":pph,"Target":target_pph}
+    print(pph_tracker)
+def query_zone(zone_id):
+    print(pph_tracker[zone_id])
+def update_zone(zone_id,key,value):
+    pph_tracker[zone_id][key] = value
+    print(pph_tracker)
+add_zone("A3","1st",220,120)
+query_zone("A3")
+update_zone("A2","Shift","1st")
