0872963eda1b150db94714b53d2f14d9fd886028
Saran1594 <mech.saravananm@gmail.com>
2026-08-26 10:21:32 -0400

    Update Day02.py

--- Week2/Day02.py
+++ Week2/Day02.py
@@ -8,6 +8,6 @@ with open("C:\\Users\\JW60\\OneDrive - Nordstrom\\Panda.csv","r", encoding="utf-
         zone["PPH"] = int(zone["PPH"])
         zone["Target_PPH"] = int(zone["Target_PPH"])
 sorter_zone = sorted(zones,key= lambda zone:zone["PPH"])
-top_zones = sorter_zone[-3:]
-top_zones_reversed = top_zones[::-1]
+top_zones = sorter_zone[-3:] # This gets the top 3 best performers
+top_zones_reversed = top_zones[::-1] # This sorts them
 pprint(top_zones_reversed)
